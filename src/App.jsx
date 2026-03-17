d8bac80bce48c2836db7563deeadbefe8d423354
Riyadulislamratul <riadul1810833319@gmail.com>
2026-03-17 23:21:35 +0600

    everything created

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
+
+
+const App = () => {
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
---
+++ src/pages/ProjectSinglePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProjectSinglePage = () => {
+  return (
+    <div>ProjectSinglePage</div>
+  )
+}
+
+export default ProjectSinglePage
