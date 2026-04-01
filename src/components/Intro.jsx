84cf231292cb340164184af6afc5d0b8635d2df2
Riyadulislamratul <riadul1810833319@gmail.com>
2026-04-02 00:08:25 +0600

    fix: align vite and plugin versions

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Section from './Section'
+import Container from './Container'
 
 const Intro = () => {
   return (
-    <div>Intro</div>
+    <Section>
+      <Container>
+        <h1 className='text-3xl font-bold'>My name is Riyadul Islam Ratul</h1>
+        <h2 className='text-2xl font-bold'>Frontend Developer</h2>
+        
+      </Container>
+    </Section>
   )
 }
 
