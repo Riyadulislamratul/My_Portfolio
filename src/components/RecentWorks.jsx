62eb414b0fb84c48e0dad4e186a19847d988bfca
Riyadulislamratul <riadul1810833319@gmail.com>
2026-04-11 21:33:16 +0600

    Recent Works animation added

--- src/components/RecentWorks.jsx
+++ src/components/RecentWorks.jsx
@@ -4,40 +4,48 @@ import Container from "./Container";
 import { Link } from "react-router";
 import PI_1 from "../assets/project-1.png";
 import PI_2 from "../assets/project-2.png";
-import {  ArrowUpRightIcon } from "lucide-react";
+import { ArrowUpRightIcon } from "lucide-react";
+import Reveal from "./Reveal";
 
 const RecentWorks = () => {
   return (
     <section className="bg-orange">
       <Container>
         <div className="py-12 md:py-16 lg:py-20">
-  <div className="text-center max-w-2xl mx-auto">
-    <p className="text-brand font-syne text-lg md:text-xl pb-2 font-bold">
-      Portfolio
-    </p>
-    <h1 className="text-3xl md:text-4xl lg:text-6xl font-syne font-bold text-heading pb-6 md:pb-10">
-      My Recent Work
-    </h1>
-  </div>
+          <Reveal direction="down">
+            <div className="text-center max-w-2xl mx-auto">
+            <p className="text-brand font-syne text-lg md:text-xl pb-2 font-bold">
+              Portfolio
+            </p>
+            <h1 className="text-3xl md:text-4xl lg:text-6xl font-syne font-bold text-heading pb-6 md:pb-10">
+              My Recent Work
+            </h1>
+          </div>
+          </Reveal>
 
-  <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 lg:gap-10">
-<ProjectCard
-  project={{
-    image: PI_1,
-    title: "Full Ecommerce Website",
-    description: "Description of project 1",
-    link: "https://e-commerce-five-navy-79.vercel.app/",
-  }}
-/>
-<ProjectCard
-  project={{
-    image: PI_2,
-    title: "Modevo Fashion Website",
-    link: "https://modevo-project-with-react.vercel.app/",
-  }}
-/>
-  </div>
-</div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 lg:gap-10">
+            <Reveal direction="left">
+              <ProjectCard
+              project={{
+                image: PI_1,
+                title: "Full Ecommerce Website",
+                description: "Description of project 1",
+                link: "https://e-commerce-five-navy-79.vercel.app/",
+              }}
+            />
+            </Reveal>
+            <Reveal direction="right">
+              <ProjectCard
+                project={{
+                  image: PI_2,
+                  title: "Modevo Fashion Website",
+                  description: "Description of project 2",
+                  link: "https://modevo-project-with-react.vercel.app/",
+                }}
+              />
+            </Reveal>
+          </div>
+        </div>
       </Container>
     </section>
   );
@@ -47,14 +55,13 @@ export default RecentWorks;
 
 const ProjectCard = ({ project }) => {
   return (
-    <div className="w-full">
-
+    <div className="w-full  ">
       {/* Image */}
-      <div className="w-full aspect-[4/3] rounded-2xl overflow-hidden mb-4 md:mb-6 group">
+      <div className="w-full rounded-2xl overflow-hidden mb-4 md:mb-6 group h-[380px]">
         <img
           src={project.image}
           alt={project.title}
-          className="w-full h-full object-cover transition duration-500 group-hover:scale-105"
+          className="w-full h-full transition duration-500 group-hover:scale-105"
         />
       </div>
 
