cb19fe221cbecda30358d9e1bcfc7cb2c2129355
Riyadulislamratul <riadul1810833319@gmail.com>
2026-09-04 21:40:37 +0600

    Mainlayout updated(Scroll top problem fixed)

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -4,11 +4,13 @@ import { Outlet } from 'react-router'
 import Footer from './Footer'
 import ScrollProgress from './ScrollProgress'
 import Cursor from './Cursor'
+import ScrollToTop from './ScrollToTop'
 
 const MainLayout = () => {
   return (
     <>
     <Cursor />
+    <ScrollToTop />
     <ScrollProgress />
     <Header />
     <Outlet />
